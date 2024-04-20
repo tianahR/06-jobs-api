2d83691ec2583625c986f49cd43dee4aeec91237
tianahR <farahrabenjam@yahoo.com>
2024-04-20 16:37:44 -0400

    Hashing password with bcryptjs

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,25 @@
+//inport model
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError } = require('../errors')
+const bcrypt = require('bcryptjs')
+
 const register = async (req, res) => {
-   res.send('register user')
+    
+  const { name, email, password } = req.body
+
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password,salt)
+  const tempUser  = {name,email,password:hashedPassword}
+
+//   if (!name || !email || !password || name === '' || password === '' || email ==='') {
+//     throw new BadRequestError('Please provide name, email and password')
+//   }
+ 
+    //create User 
+   const user = await User.create({ ...tempUser })
+
+   res.status( StatusCodes.CREATED).json({user})
 };
 
 const login = async (req, res) => {
