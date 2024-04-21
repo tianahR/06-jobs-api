ac466003eb7311636615d48d407b609697ff0e92
tianahR <farahrabenjam@yahoo.com>
2024-04-21 15:29:03 -0400

    Added functionality for Creating job and getting all jobs created by same user

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,14 +1,23 @@
+const Job = require('../models/Job')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError, NotFoundError } = require('../errors')
+
+
+//find all jobs created by one user 
 const getAllJobs = async (req, res) => {
-    res.send('get all jobs ')
- };
+    const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt')
+    res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
+}
  
  const getJob = async (req, res) => {
      res.send(' get a specific job')
  };
 
  const createJob = async (req, res) => {
-    res.json(req.user)
-};
+    req.body.createdBy = req.user.userId
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({ job })
+  }
 
 const updateJob = async (req, res) => {
     res.send(' update a job')
